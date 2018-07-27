3e2f12c2459533a86a9aee95a61f6709fd89ab05
Rik Brutsaert <hb31@cornell.edu>
2018-07-26 18:10:17 -0700

    setting up heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ const port=process.env.PORT||3000;
 var app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
