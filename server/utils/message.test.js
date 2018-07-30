dfbd120a5bccca594b508e9f491d4b2d9514c8e5
Rik Brutsaert <hb31@cornell.edu>
2018-07-30 11:46:55 -0400

    commiting changes

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,13 @@
 var expect = require("expect");
 
-var { generateMessage } = require("./message");
+var { generateMessage, generateLocationMessage } = require("./message");
 
-const from = "testFrom";
-const text = "testText";
+
 
 describe("generateMessage", () => {
   it("should generate correct message object", () => {
+    const from = "testFrom";
+    const text = "testText";
     //store res in variable
     const result = generateMessage(from, text);
     //assert from match
@@ -15,5 +16,17 @@ describe("generateMessage", () => {
     expect(result.text).toBe(text);
     //assert createdat is number
     expect(typeof result.createdAt).toBe("number");
+  });
+});
+
+describe("generateLocationMessage", () => {
+  const testFrom = "testFrom";
+  const testLat = 123;
+  const testLong = 235;
+
+  it("should genereate correct location object", () => {
+    const result = generateLocationMessage(testFrom, testLat, testLong);
+    expect(result.from).toBe(testFrom);
+    expect(result.url).toBe(`https://www.google.com/maps?q=123,235`)
   });
 });
